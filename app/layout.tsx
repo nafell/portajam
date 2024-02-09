41438c081b29214e958a359716f5aa1b6d022987
nafell <pixel68.43@gmail.com>
2024-02-09 11:18:57 +0900

    code block font, next version updated to 14

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import Footer from '@/components/Footer';
 import Nav from '@/components/Nav';
 import './globals.css';
 import styles from './layout.module.css';
+import { Noto_Sans_JP, Source_Code_Pro } from 'next/font/google';
+
+const noto_sans_jp = Noto_Sans_JP({ subsets: ['latin'] });
+const source_code_pro = Source_Code_Pro({ subsets: ['latin'], variable: '--code-font' });
 
 export const metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
@@ -33,7 +37,9 @@ export default async function RootLayout({ children }: Props) {
       <body>
         <Header />
         <Nav tags={tags.contents} />
-        <main className={styles.main}>{children}</main>
+        <main className={`${styles.main} ${noto_sans_jp.className} ${source_code_pro.variable}`}>
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
